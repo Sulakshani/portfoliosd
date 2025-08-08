8f62da6be2e210c7433d9bc41f3b1244c8936d1e
Sulakshani Dissanayake <sulakshanidissanayake@gmail.com>
2025-08-09 01:08:52 +0530

    Blog changes

--- components/Home/Contact/Contact.tsx
+++ components/Home/Contact/Contact.tsx
@@ -9,28 +9,27 @@ const Contact = () => {
         {/* Text content */}
         <div>
           <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-gray-200">
-            Schedule a call with me to see if I can help
+            Have a project in mind or want to collaborate? 
           </h1>
           <p className="text-gray-400 mt-6 text-base sm:text-lg">
-            Reach out to me today and let’s discuss how I can help you achieve
-            your goals.
+            I'd love to hear from you. Let's create something amazing together.
           </p>
           {/* info */}
           <div className="mt-7">
             <div className="flex items-center space-x-3 mb-4">
               <BiPhone className="w-9 h-9 text-cyan-300" />
-              <p className="text-xl font-bold text-gray-400">+9843582345</p>
+              <p className="text-xl font-bold text-gray-400">+94715342627 / +94750342627</p>
             </div>
             <div className="flex items-center space-x-3 mb-4">
               <BiEnvelope className="w-9 h-9 text-cyan-300" />
               <p className="text-xl font-bold text-gray-400">
-                hello@example.com
+                sulakshanidissanayake@gmail.com
               </p>
             </div>
             <div className="flex items-center space-x-3 mb-4">
               <BiMap className="w-9 h-9 text-cyan-300" />
               <p className="text-xl font-bold text-gray-400">
-                Dhaka, Bangladesh
+                Moratuwa, Srilanka
               </p>
             </div>
           </div>
